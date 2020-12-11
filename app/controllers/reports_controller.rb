d414fa7e3e2a698011d99d7019f994935c550028
Joao Vieira <joo.vieira123@gmail.com>
2020-12-11 16:53:12 +0100

    implemented server authentication for the right credentials of code_name and access token

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,16 @@
 class ReportsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :athenticate_server
   def handle
     render json: { message: 'testing' }
+  end
+
+  def athenticate_server
+    code_name = request.headers['X-Server-CodeName']
+    server = Server.find_by(code_name: code_name)
+    access_token = request.headers['X-Server-Token']
+    unless server && server.access_token == access_token
+      render json: { message: 'Wrong Credentials' }
+    end
   end
 end
