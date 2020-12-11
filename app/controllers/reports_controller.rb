77e1cb37b8def957223f524e540cc9bd7415a9d6
Joao Vieira <joo.vieira123@gmail.com>
2020-12-11 18:04:19 +0100

    created logic to parse the csv file and update or create a new lock and its entry

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,30 @@
+require 'csv'
 class ReportsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :athenticate_server
   def handle
-    render json: { message: 'testing' }
+    if Rails.env.development?
+      Entry.delete_all
+      Lock.delete_all
+    end
+    report = params[:report].open
+
+    csv_options = { col_sep: ',', headers: :first_row }
+
+    CSV.parse(report, csv_options) do |timestamp, lock_id, kind, status_change|
+      lock = Lock.find_by_id(lock_id[1])
+      if lock
+        lock.status = status_change[1]
+        lock.save
+      else
+        lock = Lock.create(id: lock_id[1], kind: kind[1], status: status_change[1])
+      end
+
+      Entry.create(timestamp: timestamp[1], status_change: status_change[1], lock: lock)
+    end
+
+    render json: { message: "Congrats, your report has been processed.
+      Now you have #{Lock.count} locks and #{Entry.count} entries." }
   end
 
   def athenticate_server
